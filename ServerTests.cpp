e3b4075b22f5254c7d662f2adbd5afb1af2d702f
Alexey Martushev <a.a.martushev@gmail.com>
2025-12-17 22:49:50 +0700

    HMAC-9. Add stability test

--- ServerTests.cpp
+++ ServerTests.cpp
@@ -229,3 +229,48 @@ TEST(ServerTest, MessageLenIsExceeded) {
 
     ASSERT_EQ(reply["error"].as_string(), "too_large_msg"s);
 }
+
+TEST(ServerTest, Stability) {
+
+    hmac_service::Config config;
+    ASSERT_TRUE(config.Upload());
+
+    hmac_service::Server server;
+
+    const uri uri(std::string(config.GetUri()));
+    http::client::http_client client(uri);
+
+    json::value request_json;
+    request_json["msg"] = json::value::string("two words...");
+
+    http::status_code status;
+    json::value reply;
+
+	client.request(http::methods::POST, U("/sign"), request_json)
+        .then([&status, &reply](pplx::task<http::http_response> task) {
+            http::http_response response = task.get();
+            status = response.status_code();
+            reply = response.extract_json().get();
+        })
+        .wait();
+
+    ASSERT_EQ(status, http::status_codes::OK);
+    ASSERT_TRUE(reply.has_field("signature"));
+
+    const std::string signature = reply["signature"].as_string();
+
+    for (std::int16_t i = 0; i < 1000; ++i) {
+        client.request(http::methods::POST, U("/sign"), request_json)
+        .then([&status, &reply](pplx::task<http::http_response> task) {
+            http::http_response response = task.get();
+            status = response.status_code();
+            reply = response.extract_json().get();
+        })
+        .wait();
+
+        ASSERT_EQ(status, http::status_codes::OK);
+        ASSERT_TRUE(reply.has_field("signature"));
+
+        ASSERT_EQ(signature, reply["signature"].as_string());
+    }
+}
