2f8991a6c1167c3b4c667ac7ad73c1dd064f3030
Alexey Martushev <a.a.martushev@gmail.com>
2025-12-18 00:53:56 +0700

    Fix simple logs in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,18 @@
 int main(int argc, char* argv[]) {
     try {
         hmac_service::Server server;
+        if (!server.IsListening()) {
+            std::cerr << "Server run is failed" << std::endl;
+            return 1;
+        }
 		
-        std::cout << "Press Enter to stop server\n";
+        std::cout << "Press Enter to stop server" << std::endl;
         std::string dummy;
         std::getline(std::cin, dummy);
     }
     catch (const std::exception & e) {
-        std::cerr << "Exception: " << e.what() << " was thrown\n";
+        std::cerr << "Exception: " << e.what() << " was thrown" << std::endl;
+        return 1;
     }
     return 0;
 }
